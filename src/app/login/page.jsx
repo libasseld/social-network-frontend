96cd0402d20e5eb331f2d817c0885b741d9930d3
libasseld <50105345+libasseld@users.noreply.github.com>
2025-01-11 09:40:10 +0000

    Logo changed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -44,8 +44,8 @@ export default function Login() {
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+          className="mx-auto h-40 w-auto"
+          src="/EPF_logo_2021.png"
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -30,8 +30,11 @@ export default function Register() {
     return (
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
-        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Inscription </h2>
+      <img
+          className="mx-auto h-40 w-auto"
+          src="/EPF_logo_2021.png"
+          alt="Your Company"
+        />        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Inscription </h2>
       </div>
     
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
